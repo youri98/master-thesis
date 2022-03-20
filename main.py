05ca924e9aa4259d9b113b041cd77a4e6417c1a8
youri98 <yourivis@me.com>
2022-03-20 11:27:27 +0100

    setup env

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
-print('gekll')
+import sys
+import numpy as np
+from ale_py import ALEInterface
+from ale_py.roms import Breakout, MontezumaRevenge
+import random
+from random import randrange
+import matplotlib.pyplot as plt
+import cv2
+from IPython.display import HTML
+import matplotlib.animation as animation
+from collections import deque
+from queue import Queue
+from IPython import display
+from utils import rebin, visualize, setup_environment
+
+
+random.seed(42)
+
+ROM = "MontezumaRevenge"
+
+
+def run_game(env: ALEInterface, record: bool = False, downscaling: int = 5, max_frames: int = 100):
+    actions = env.getMinimalActionSet()
+    num_actions = len(actions)
+
+    recording = []
+    total_reward = 0
+    while not env.game_over() and env.getEpisodeFrameNumber() < max_frames:
+        # get state
+        observation = env.getScreenGrayscale()
+        downscaled_obs = rebin(
+            observation, (observation.shape[0] // downscaling, observation.shape[1] // downscaling))  # downscale
+
+        # select action via model
+        a = actions[randrange(num_actions)]
+
+        # get reward
+        reward = env.act(a)
+        total_reward += reward
+
+        if record:
+            recording.append(downscaled_obs)
+
+    if record:
+        visualize(recording)
+
+    print(f'Episode ended with score: {total_reward}')
+
+
+def main():
+    env = setup_environment(ROM)
+    run_game(env)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,42 @@
+from ale_py import ALEInterface
+from ale_py.roms import Breakout, MontezumaRevenge
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+import typing
+import numpy as np
+from typing import List, Set, Dict, Tuple, Optional
+
+
+def rebin(a: np.ndarray, shape: Tuple[int, int]):
+    # downscales 2D-array with mean
+    sh = shape[0], a.shape[0]//shape[0], shape[1], a.shape[1]//shape[1]
+    return a.reshape(sh).mean(-1).mean(1)
+
+
+def visualize(recording: np.ndarray, fps: int = 120):
+
+    fig = plt.figure(1, figsize=(8, 8))
+
+    def animate(i):
+        fig.clf()
+        plt.imshow(recording[i], cmap='gray', aspect='equal')
+        plt.axis('off')
+        fig.canvas.draw()
+
+    anim = animation.FuncAnimation(
+        fig, animate, interval=1, frames=len(recording))
+    writer = animation.FFMpegWriter(fps=fps)
+
+    print(f"saving {len(recording)//fps} seconds of recording...")
+    start = time.time()
+    anim.save("recordings/recording.mp4", writer=writer, dpi=50)
+    print(f"done saving! It took {time.time() - start} seconds")
+
+
+def setup_environment(rom: str):
+    game_dict = {"MontezumaRevenge": MontezumaRevenge,
+                 "Breakout": Breakout}
+    ale = ALEInterface()
+    ale.loadROM(game_dict[rom])
+    return ale
