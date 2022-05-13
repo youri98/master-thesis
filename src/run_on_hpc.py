0831a982bcdb68d70e1a96b3f79b941981d79e71
youri98 <yourivis@me.com>
2022-05-13 16:38:48 +0200

    revert to before float16

--- src/run_on_hpc.py
+++ src/run_on_hpc.py
@@ -1,27 +1,12 @@
-#!/usr/bin/env python3
-#SBATCH --partition=mcs.gpu.q
-#SBATCH --output=openme.out
-#SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --error=slurm-%j.err
-#SBATCH --time=2:00:00
-#SBATCH --gres
-
 import sys, os
 sys.path.append(os.getcwd())
 
 from config import get_params
 from main import train_model
 import wandb
-import torch
 
 
 if __name__ == '__main__':
     config = get_params()
-
-    # run 1
-    config["total_rollouts"] = int(50)
-    config["n_workers"] = 16
-
     train_model(config, run_from_hpc=False)
     wandb.finish()
