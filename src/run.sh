9d433a2c5f1f0e1b45a00dbb643f7aca3c6c4924
youri98 <yourivis@me.com>
2022-05-17 12:22:28 +0200

    ready for hpc

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 #SBATCH --partition=mcs.gpu.q
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=56
 #SBATCH --error=slurm-%j.err
 #SBATCH --output=slurm-%j.out
-#SBATCH --time=2:00:00
-#SBATCH --gres=gpu:teslav100
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:A
 
 module load python3
 module load cuda10.2/toolkit/10.2.89
 module load mpi
 
-python run_on_hpc.py --total_rollouts 50 --record --algo RND
+python run_on_hpc.py --total_rollouts 15000 --record --algo RND
