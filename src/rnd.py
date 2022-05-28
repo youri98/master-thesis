d5b7c0e7cf2911fad0f72ffa1b9e9797e1d86d33
youri98 <yourivis@me.com>
2022-05-28 16:29:35 +0200

    memory gpu

--- src/rnd.py
+++ src/rnd.py
@@ -63,6 +63,7 @@ class RND:
             state = np.expand_dims(state, 0)
         state = from_numpy(state).to(self.device)
 
+        torch.cuda.empty_cache() 
         with torch.no_grad():
             outputs = self.current_policy(state)
             int_value, ext_value, action_prob = outputs
@@ -123,7 +124,7 @@ class RND:
                                                                                                                advs=advs,
                                                                                                                log_probs=log_probs,
                                                                                                                next_states=total_next_obs):
-                # print("inside batch ", state.shape)
+                print("inside batch ", state.shape)
                 outputs = self.current_policy(state)
                 int_value, ext_value, action_prob = outputs
                 dist = Categorical(action_prob)
