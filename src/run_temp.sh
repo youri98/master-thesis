6c821bd9ebdd41206bca72a2e5d9f09b907b6535
youri98 <yourivis@me.com>
2022-05-28 13:28:01 +0200

    temp

---
+++ src/run_temp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#SBATCH --partition=mcs.gpu.q
+#SBATCH --nodes=1
+#SBATCH --ntasks=56
+#SBATCH --error=slurm-%j.err
+#SBATCH --output=slurm-%j.out
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:4
+
+module load python3
+module load cuda10.2/toolkit/10.2.89
+module load mpi
+
+python temp.py --total_rollouts 16 --record --algo RND
