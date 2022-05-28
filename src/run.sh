9fb1763f18932527b911b97434dc3fe2a63bceac
youri98 <yourivis@me.com>
2022-05-28 15:45:11 +0200

    i guess usable

--- src/run.sh
+++ src/run.sh
@@ -7,8 +7,6 @@
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:4
 
-module load python3
 module load cuda10.2/toolkit/10.2.89
-module load mpi
 
 python run_on_hpc.py --total_rollouts 150 --record --algo RND
