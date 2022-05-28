2f04e078f56270a2d8320b18cf0e9f11cc2f66e8
youri98 <yourivis@me.com>
2022-05-28 13:31:49 +0200

    changed model

--- src/rnd_models.py
+++ src/rnd_models.py
@@ -11,46 +11,43 @@ def conv_shape(input, kernel_size, stride, padding=0):
 
 
 class PolicyModel(nn.Module, ABC):
-
     def __init__(self, state_shape, n_actions):
         super(PolicyModel, self).__init__()
         self.state_shape = state_shape
         self.n_actions = n_actions
 
-        c, w, h = state_shape
-        self.conv1 = nn.Conv2d(in_channels=c, out_channels=32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3,
-                               stride=1)  # Nature paper -> kernel_size = 3, OpenAI repo -> kernel_size = 4
+        color, w, h = state_shape
+        conv1_out_shape = conv_shape((w, h), 8, 4)
+        conv2_out_shape = conv_shape(conv1_out_shape, 4, 2)
+        conv3_out_shape = conv_shape(conv2_out_shape, 3, 1)
 
-        conv1_out_w = conv_shape(w, 8, 4)
-        conv1_out_h = conv_shape(h, 8, 4)
-        conv2_out_w = conv_shape(conv1_out_w, 4, 2)
-        conv2_out_h = conv_shape(conv1_out_h, 4, 2)
-        conv3_out_w = conv_shape(conv2_out_w, 3, 1)
-        conv3_out_h = conv_shape(conv2_out_h, 3, 1)
+        flatten_size = conv3_out_shape[0] * conv3_out_shape[1] * 64
 
-        flatten_size = conv3_out_w * conv3_out_h * 64
+        self.seq = nn.Sequential(
+            nn.Conv2d(color, 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64,  kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(start_dim=1),
+            nn.Linear(flatten_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, 448),
+            nn.ReLU(),
+        )
 
-        self.fc1 = nn.Linear(in_features=flatten_size, out_features=256)
-        self.fc2 = nn.Linear(in_features=256, out_features=448)
+        self.policy = nn.Linear(448, self.n_actions)
+        self.int_value = nn.Linear(448, 1)
+        self.ext_value = nn.Linear(448, 1)
 
-        self.extra_value_fc = nn.Linear(in_features=448, out_features=448)
-        self.extra_policy_fc = nn.Linear(in_features=448, out_features=448)
+        self.extra_value_fc = nn.Linear(448, 448)
+        self.extra_policy_fc = nn.Linear(448, 448)
 
-        self.policy = nn.Linear(in_features=448, out_features=self.n_actions)
-        self.int_value = nn.Linear(in_features=448, out_features=1)
-        self.ext_value = nn.Linear(in_features=448, out_features=1)
-
-        for layer in self.modules():
-            if isinstance(layer, nn.Conv2d):
+        for layer in self.seq.children():
+            if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
                 nn.init.orthogonal_(layer.weight, gain=np.sqrt(2))
                 layer.bias.data.zero_()
-
-        nn.init.orthogonal_(self.fc1.weight, gain=np.sqrt(2))
-        self.fc1.bias.data.zero_()
-        nn.init.orthogonal_(self.fc2.weight, gain=np.sqrt(2))
-        self.fc2.bias.data.zero_()
 
         nn.init.orthogonal_(self.extra_policy_fc.weight, gain=np.sqrt(0.1))
         self.extra_policy_fc.bias.data.zero_()
@@ -64,25 +61,19 @@ class PolicyModel(nn.Module, ABC):
         nn.init.orthogonal_(self.ext_value.weight, gain=np.sqrt(0.01))
         self.ext_value.bias.data.zero_()
 
-    def forward(self, inputs):
-        x = inputs / 255.
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        # x = x.contiguous()
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x_v = x + F.relu(self.extra_value_fc(x))
+    def forward(self, state):
+        #state = np.divide(state, 255., out=state, casting="unsafe")
+        x = state / 255.
+        x = self.seq(x)
+        x_value = x + F.relu(self.extra_value_fc(x))
         x_pi = x + F.relu(self.extra_policy_fc(x))
-        int_value = self.int_value(x_v)
-        ext_value = self.ext_value(x_v)
+        int_value = self.int_value(x_value)
+        ext_value = self.ext_value(x_value)
         policy = self.policy(x_pi)
         probs = F.softmax(policy, dim=1)
         dist = Categorical(probs)
 
         return dist, int_value, ext_value, probs
-
 
 class TargetModel(nn.Module, ABC):
 
