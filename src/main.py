8bb98c626974befb21af43c2139712bad1c3c65f
youri98 <yourivis@me.com>
2022-06-14 12:18:59 +0200

    with static methds

--- src/main.py
+++ src/main.py
@@ -32,6 +32,7 @@ from collections import deque
 class PooledGA(pygad.GA):
 
     def cal_pop_fitness(self):
+        global agent, config, logger, pool
 
         print("calculating population fitness...")
         if not hasattr(self, "frames"):
@@ -41,11 +42,10 @@ class PooledGA(pygad.GA):
         else:
             self.iteration += 1
         
-        global pool, agent, config, logger
 
         # play in env
         logger.time_start()
-        output = pool.map(fitness_wrapper, self.population)
+        output = pool.map(GAfunctions.fitness_wrapper, self.population)
         logger.time_stop("Env Time")
 
         total_ext_rewards, episode_logs, observations, int_values, ext_values, next_int_values, next_ext_values, dones = zip(*output)
@@ -101,144 +101,147 @@ class PooledGA(pygad.GA):
         print(f"pop fitness: {pop_fitness}, frames {self.frames}, rnd_loss: {rnd_loss}")
         return pop_fitness
 
-def callback_generation(ga_instance):
-    print("on_generation()")
-
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solutions_fitness))
-    ga_instance.best_solutions_fitness = []
-
-def on_fitness(ga_instance, population_fitness):
-    print("on_fitness()")
-
-def on_parents(ga_instance, selected_parents):
-    print("on_parents()")
-    logger.time_start()
-
-def on_crossover(ga_instance, offspring_crossover):
-    print("on_crossover()")
-    logger.time_stop("Crossover Time")
-    logger.time_start()
-
-
-def on_mutation(ga_instance, offspring_mutation):
-    print("on_mutation()")
-    logger.time_stop("Mutation Time")
-
-
-def fitness_wrapper(solution):
-    return fitness_func(solution, 0)
-
-def run_workers_env_step(worker, conn):
-    worker.step(conn)
-
-def fitness_func(solution, sol_idx):
-    global envs, config, agent
-
-    current_pool_id = multiprocessing.current_process()._identity[0] - 2 # dont get why its 2 tm 9
-    policy_model_weights_dict = pygad.torchga.model_weights_as_dict(model=agent.current_policy, weights_vector=solution)
-    agent.current_policy.load_state_dict(policy_model_weights_dict)
-
-    # initialize env
-    episode_ext_reward, total_obs, done, t = [], [], False, 1
-    total_int_values, total_ext_values, total_next_int_values, total_next_ext_values, total_dones = [], [], [], [], []
+class GAfunctions():
     
-    state_shape = config["state_shape"]
-    env = envs[current_pool_id] 
-    state = env.reset() # firtst observation
+    @staticmethod
+    def callback_generation(ga_instance):
+        print("on_generation()")
 
-    _stacked_states = np.zeros(state_shape, dtype=np.uint8) # stacking 4 observations
-    _stacked_states = stack_states(_stacked_states, state, True)
+        print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+        print("Fitness    = {fitness}".format(fitness=ga_instance.best_solutions_fitness))
+        ga_instance.best_solutions_fitness = []
     
-    # rollout length / until dead
-    while t <= config["max_frames_per_episode"] and not done and t <= 700:
-        state = torch.from_numpy(_stacked_states).to(agent.device)
+    @staticmethod
+    def on_fitness(ga_instance, population_fitness):
+        print("on_fitness()")
+    
+    @staticmethod
+    def on_parents(ga_instance, selected_parents):
+        print("on_parents()")
+        logger.time_start()
+    
+    @staticmethod
+    def on_crossover(ga_instance, offspring_crossover):
+        print("on_crossover()")
+        logger.time_stop("Crossover Time")
+        logger.time_start()
+
+    @staticmethod
+    def on_mutation(ga_instance, offspring_mutation):
+        print("on_mutation()")
+        logger.time_stop("Mutation Time")
+
+    @staticmethod   
+    def fitness_wrapper(solution):
+        return GAfunctions.fitness_func(solution, 0)
+
+    @staticmethod
+    def fitness_func(solution, sol_idx):
+        global envs, config, agent
+
+        current_pool_id = multiprocessing.current_process()._identity[0] - 2 # dont get why its 2 tm 9
+        policy_model_weights_dict = pygad.torchga.model_weights_as_dict(model=agent.current_policy, weights_vector=solution)
+        agent.current_policy.load_state_dict(policy_model_weights_dict)
+
+        # initialize env
+        episode_ext_reward, total_obs, done, t = [], [], False, 1
+        total_int_values, total_ext_values, total_next_int_values, total_next_ext_values, total_dones = [], [], [], [], []
         
-        with torch.no_grad():
-            int_value, ext_value, action_prob = agent.current_policy(torch.unsqueeze(state.type(torch.float), 0))
-            dist = Categorical(action_prob)
-            action = dist.sample()
-            log_prob = dist.log_prob(action)
+        state_shape = config["state_shape"]
+        env = envs[current_pool_id] 
+        state = env.reset() # firtst observation
 
-        # action, int_value, ext_value, log_prob, action_prob = agent.get_actions_and_values(_stacked_states, batch=True)
-        next_state, r, done, info = env.step(action)
+        _stacked_states = np.zeros(state_shape, dtype=np.uint8) # stacking 4 observations
+        _stacked_states = stack_states(_stacked_states, state, True)
+        
+        # rollout length / until dead
+        while t <= config["max_frames_per_episode"] and not done and t <= 700:
+            state = torch.from_numpy(_stacked_states).to(agent.device)
+            
+            with torch.no_grad():
+                int_value, ext_value, action_prob = agent.current_policy(torch.unsqueeze(state.type(torch.float), 0))
+                dist = Categorical(action_prob)
+                action = dist.sample()
+                log_prob = dist.log_prob(action)
 
-        if t % config["max_frames_per_episode"] == 0:
-            done = True
+            # action, int_value, ext_value, log_prob, action_prob = agent.get_actions_and_values(_stacked_states, batch=True)
+            next_state, r, done, info = env.step(action)
 
-
-        _stacked_states = stack_states(_stacked_states, next_state, False)
-        next_obs = _stacked_states[-1, ...]
-
-        next_state = torch.from_numpy(_stacked_states).to(agent.device)
-
-        with torch.no_grad():
-            next_int_value, next_ext_value, _= agent.current_policy(torch.unsqueeze(next_state.type(torch.float), 0))
-
-        if "episode" in info and current_pool_id == 0 and done:
-            visited_rooms = info["episode"]["visited_room"]
-            episode = info["episode"]
-
-            episode_logs = {"Ep Visited Rooms": list(visited_rooms), "Episode Ext Reward": sum(episode_ext_reward)}
-
-        t += 1
-
-        episode_ext_reward.append(r)
-        total_obs.append(next_obs)
-        total_int_values.append(int_value)
-        total_ext_values.append(ext_value)
-        total_next_int_values.append(next_int_value)
-        total_next_ext_values.append(next_ext_value)
-        total_dones.append(done)
+            if t % config["max_frames_per_episode"] == 0:
+                done = True
 
 
-    total_obs = np.array(total_obs)
-    total_obs = np.expand_dims(total_obs, 1)
+            _stacked_states = stack_states(_stacked_states, next_state, False)
+            next_obs = _stacked_states[-1, ...]
 
+            next_state = torch.from_numpy(_stacked_states).to(agent.device)
 
-    if "episode_logs" in locals():
-        return episode_ext_reward, episode_logs, total_obs, total_int_values, total_ext_values, total_next_int_values, total_next_ext_values, total_dones
-    else:
-        return episode_ext_reward, None, total_obs, total_int_values, total_ext_values, total_next_int_values, total_next_ext_values, total_dones
+            with torch.no_grad():
+                next_int_value, next_ext_value, _= agent.current_policy(torch.unsqueeze(next_state.type(torch.float), 0))
 
+            if "episode" in info and current_pool_id == 0 and done:
+                visited_rooms = info["episode"]["visited_room"]
+                episode = info["episode"]
 
+                episode_logs = {"Ep Visited Rooms": list(visited_rooms), "Episode Ext Reward": sum(episode_ext_reward)}
 
+            t += 1
 
+            episode_ext_reward.append(r)
+            total_obs.append(next_obs)
+            total_int_values.append(int_value)
+            total_ext_values.append(ext_value)
+            total_next_int_values.append(next_int_value)
+            total_next_ext_values.append(next_ext_value)
+            total_dones.append(done)
 
-torch.autograd.set_detect_anomaly(True)
+        total_obs = np.array(total_obs)
+        total_obs = np.expand_dims(total_obs, 1)
 
-config = get_params()
-agent = APE(**config)
-logger = Logger(agent, **config)
-logger.log_config_params()
+        if "episode_logs" in locals():
+            return episode_ext_reward, episode_logs, total_obs, total_int_values, total_ext_values, total_next_int_values, total_next_ext_values, total_dones
+        else:
+            return episode_ext_reward, None, total_obs, total_int_values, total_ext_values, total_next_int_values, total_next_ext_values, total_dones
 
 
 
-envs = [make_atari(config["env"], config["max_frames_per_episode"]) for _ in range(config["n_individuals_per_gen"])] # change this outside call    
-torch_ga = pygad.torchga.TorchGA(model=agent.current_policy, num_solutions=config["n_individuals_per_gen"])
+def main():
+    global config, agent, logger, pool, envs
+    # torch.autograd.set_detect_anomaly(True)
 
-initial_population = torch_ga.population_weights  # Initial population of network weights
+    config = get_params()
+    agent = APE(**config)
+    logger = Logger(agent, **config)
+    logger.log_config_params()
 
 
-ga_instance = PooledGA(num_generations=config["num_generations"],
-                    num_parents_mating=config["num_parents_mating"],
-                    parent_selection_type=config["parent_selection_type"],
-                    crossover_type=config["crossover_type"],
-                    mutation_type=config["mutation_type"],
-                    mutation_percent_genes=config["mutation_percent_genes"],
-                    keep_parents=config["keep_parents"],
-                    initial_population=initial_population,
-                    fitness_func=fitness_func,
-                    on_generation=callback_generation,
-                    on_parents=on_parents,
-                    on_mutation=on_mutation,
-                    on_fitness=on_fitness,
-                    on_crossover=on_crossover,
-                    save_best_solutions=False) # might use this for GAETL
-                    # sol_per_pop=10,
-                    # num_genes=300)
+    envs = [make_atari(config["env"], config["max_frames_per_episode"]) for _ in range(config["n_individuals_per_gen"])] # change this outside call    
+    torch_ga = pygad.torchga.TorchGA(model=agent.current_policy, num_solutions=config["n_individuals_per_gen"])
 
-with Pool(processes=config["n_workers"]) as pool:
-    ga_instance.run()
+    initial_population = torch_ga.population_weights  # Initial population of network weights
 
+
+    ga_instance = PooledGA(num_generations=config["num_generations"],
+                        num_parents_mating=config["num_parents_mating"],
+                        parent_selection_type=config["parent_selection_type"],
+                        crossover_type=config["crossover_type"],
+                        mutation_type=config["mutation_type"],
+                        mutation_percent_genes=config["mutation_percent_genes"],
+                        keep_parents=config["keep_parents"],
+                        initial_population=initial_population,
+                        fitness_func=GAfunctions.fitness_func,
+                        on_generation=GAfunctions.callback_generation,
+                        on_parents=GAfunctions.on_parents,
+                        on_mutation=GAfunctions.on_mutation,
+                        on_fitness=GAfunctions.on_fitness,
+                        on_crossover=GAfunctions.on_crossover,
+                        save_best_solutions=False) # might use this for GAETL
+                        # sol_per_pop=10,
+                        # num_genes=300)
+
+    with Pool(processes=config["n_workers"]) as pool:
+        ga_instance.run()
+
+if __name__ == '__main__':
+    main()
 
