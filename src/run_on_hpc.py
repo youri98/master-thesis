89f78064397e25090f09c7520b9cf45c74e4f5fa
youri98 <yourivis@me.com>
2022-07-08 15:45:34 +0200

    debug

--- src/run_on_hpc.py
+++ src/run_on_hpc.py
@@ -2,14 +2,14 @@ import sys, os
 sys.path.append(os.getcwd())
 
 from config import get_params
-from main import noisy_tv, train_model
+from main import train_model
 import wandb
 import time
 
 if __name__ == '__main__':
     config = get_params()
     start = time.time()
-    train_model(config, add_noisy_tv=True)
+    train_model(config)
     wandb.finish()
     stop = time.strftime('%H:%M:%S', time.gmtime(time.time() - start))
     print(f"program took {stop}")
